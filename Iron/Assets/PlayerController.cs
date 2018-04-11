1346c11dee0e399db5d07157b2e7261dec78ea91
Thomas Carey <tcarey@we-connect-the-dots.org>
2018-04-11 16:58:50 -0400

    FirstPush

---
+++ Iron/Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float walkSpeed = 1.0f;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        GetComponent<Rigidbody2D>().velocity = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * walkSpeed;
+
+
+	}
+}
